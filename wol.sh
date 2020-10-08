38efe80e09c0b58a91ca813a7f267f53d9e7299c
sharkcow <sharkcow@gmx.de>
2020-10-08 18:19:18 +0200

    typo

--- wol.sh
+++ wol.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 ### BEGIN INIT INFO
-# Provides:          MeinProgramm
-# Required-Start:    
-# Required-Stop:     
+# Provides:          WOLbyWEB
+# Required-Start:
+# Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: Starts & Stops My Programm
 # Description:       Starts & Stops My Programm
 ### END INIT INFO
- 
+
 #Switch case fuer den ersten Parameter
 case "$1" in
     start)
@@ -16,13 +16,13 @@ case "$1" in
         echo "starting uwsgi WOL interface"
         uwsgi /opt/WOLbyWEB/uwsgi.ini &
         ;;
- 
+
     stop)
  #Aktion wenn stop uebergeben wird
         echo "stopping uwsgi WOL interface (killall)"
         killall uwsgi
         ;;
- 
+
     restart)
  #Aktion wenn restart uebergeben wird
         echo "restarting uwsgi WOL interface"
@@ -34,5 +34,5 @@ case "$1" in
  echo "(start|stop|restart)"
  ;;
 esac
- 
+
 exit 0
